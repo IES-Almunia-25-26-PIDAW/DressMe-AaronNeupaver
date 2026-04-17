214e8927fc7255cf32c7c558f1f7ad69fb3fdf0d
Aaron Neupaver <aaronneupaver@gmail.com>
2026-04-17 11:14:59 +0200

    dashboard v2

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -116,7 +116,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'inicio/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inicio/static'),
-]
+STATIC_URL = '/static/'
